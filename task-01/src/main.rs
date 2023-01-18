8e834d579cb0ac353ff031fb10f40284dda842be
Roman <romagritcaenko@gmail.com>
2023-01-18 16:52:22 +0200

    move input to apropiate folder

--- task-01/src/main.rs
+++ task-01/src/main.rs
@@ -1,5 +1,13 @@
 use std::fs;
 
 fn main() {
-    
+    let current_dir = std::env::current_dir().unwrap().to_str().unwrap().to_string();
+    let path = current_dir + "\\input.txt";
+    println!("Path: {}", path);
+
+    let input = fs::read_to_string("input.txt").expect("Failed to read the file");
+
+    for line in input.lines() {
+        println!("{} line", line);
+    }
 }
