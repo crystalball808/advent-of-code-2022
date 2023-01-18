7b46a5ae8e47792f26cb1fbb394f293cca04bc78
Roman <romagritcaenko@gmail.com>
2023-01-18 17:06:45 +0200

    done

--- task-01/src/main.rs
+++ task-01/src/main.rs
@@ -1,13 +1,19 @@
 use std::fs;
 
 fn main() {
-    let current_dir = std::env::current_dir().unwrap().to_str().unwrap().to_string();
-    let path = current_dir + "\\input.txt";
-    println!("Path: {}", path);
-
     let input = fs::read_to_string("input.txt").expect("Failed to read the file");
 
-    for line in input.lines() {
-        println!("{} line", line);
+    let mut summed_calories: Vec<u32> = Vec::new();
+
+    for block in input.split("\n\n") {
+        let mut calories_amount: u32 = 0;
+        for calory in block.split("\n") {
+            let calory: u32 = calory.parse().unwrap();
+            calories_amount += calory;
+        }
+        summed_calories.push(calories_amount);
     }
+
+    summed_calories.sort_by(|a, b| b.cmp(a));
+    println!("The most calories: {:?}", summed_calories[0]);
 }
