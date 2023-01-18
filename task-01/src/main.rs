4da69dd95c88191913045873d5eaa38d5419277a
Roman <romagritcaenko@gmail.com>
2023-01-18 17:12:27 +0200

    f

--- task-01/src/main.rs
+++ task-01/src/main.rs
@@ -1,3 +1,18 @@
 fn main() {
-    println!("Hello, world!");
+    let input = fs::read_to_string("input.txt").expect("Failed to read the file");
+
+    let mut summed_calories: Vec<u32> = Vec::new();
+
+    for block in input.split("\n\n") {
+        let mut calories_amount: u32 = 0;
+        for calory in block.split("\n") {
+            let calory: u32 = calory.parse().unwrap();
+            calories_amount += calory;
+        }
+        summed_calories.push(calories_amount);
+    }
+
+    summed_calories.sort_by(|a, b| b.cmp(a));
+    println!("Top three: {:?}", &summed_calories[0..3]);
+    println!("Total: {}", summed_calories[0..3].iter().sum::<u32>());
 }
