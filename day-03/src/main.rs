0bfc67e78b9133404e264059ffb68f641b36bea0
Roman <romagritcaenko@gmail.com>
2023-02-18 16:51:59 +0200

    day 3 part 2

--- day-03/src/main.rs
+++ day-03/src/main.rs
@@ -22,8 +22,8 @@ fn get_item_priority(item: char) -> u32 {
         item_ascii_code - 96
     }
 }
-fn main() {
-    let input = fs::read_to_string("input.txt").expect("Failed to read the file");
+
+fn first_part(input: &String) {
     let mut sum_of_priorities: u32 = 0;
     for rucksack in input.split("\n") {
         let length_of_compartment = rucksack.len() / 2;
@@ -31,8 +31,43 @@ fn main() {
         let second_compartment = &rucksack[length_of_compartment..];
         let shared_item = find_shared_item(first_compartment, second_compartment);
         let shared_item_priority = get_item_priority(shared_item);
-        println!("Shared: {shared_item} Priority: {shared_item_priority}");
         sum_of_priorities += shared_item_priority;
     }
-    println!("Sum of priorities: {sum_of_priorities}")
+    println!("First part: Sum of priorities: {sum_of_priorities}");
+}
+fn second_part(input: &String) {
+    let mut sum_of_priorities: u32 = 0;
+
+    let mut first_rucksack_map: HashMap<char, bool> = HashMap::new();
+    let mut second_rucksack_map: HashMap<char, bool> = HashMap::new();
+
+    for rucksack in input.split("\n") {
+        if first_rucksack_map.is_empty() {
+            for item in rucksack.chars() {
+                first_rucksack_map.insert(item, true);
+            }
+        } else if second_rucksack_map.is_empty() {
+            for item in rucksack.chars() {
+                second_rucksack_map.insert(item, true);
+            }
+        } else {
+            for item in rucksack.chars() {
+                if first_rucksack_map.contains_key(&item) && second_rucksack_map.contains_key(&item)
+                {
+                    sum_of_priorities += get_item_priority(item);
+                    first_rucksack_map.clear();
+                    second_rucksack_map.clear();
+                    break;
+                }
+            }
+        }
+    }
+
+    println!("Second part: Sum of priorities: {sum_of_priorities}")
+}
+fn main() {
+    let input = fs::read_to_string("input.txt").expect("Failed to read the file");
+
+    first_part(&input);
+    second_part(&input);
 }
