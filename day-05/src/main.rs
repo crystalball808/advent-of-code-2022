88330cd3e342517b99c5474fada7e7f487d0e55f
Roman <romagritcaenko@gmail.com>
2023-04-22 23:41:42 +0300

    day 5 part 2

--- day-05/src/main.rs
+++ day-05/src/main.rs
@@ -46,7 +46,10 @@ fn parse_stacks(stacks: &str) -> Vec<VecDeque<char>> {
 }
 
 const NUMBER_TO_INDEX: usize = 1;
-fn operate_procedure(procedure: &str, mut stacks: Vec<VecDeque<char>>) -> Vec<VecDeque<char>> {
+fn operate_procedure_first_part(
+    procedure: &str,
+    mut stacks: Vec<VecDeque<char>>,
+) -> Vec<VecDeque<char>> {
     for command in procedure.split("\n") {
         let mut command_parts = command.split(" ");
         command_parts.next();
@@ -95,6 +98,70 @@ fn operate_procedure(procedure: &str, mut stacks: Vec<VecDeque<char>>) -> Vec<Ve
 
     return stacks;
 }
+fn first_part(parsed_stacks: Vec<VecDeque<char>>, procedure: &str) -> String {
+    let stacks = operate_procedure_first_part(procedure, parsed_stacks);
+
+    let mut answer = String::new();
+
+    for mut stack in stacks {
+        let package = stack.pop_back().expect(CORRUPTED_FILE_MESSAGE);
+        answer.push(package)
+    }
+
+    return answer;
+}
+
+fn operate_procedure_second_part(
+    procedure: &str,
+    mut stacks: Vec<VecDeque<char>>,
+) -> Vec<VecDeque<char>> {
+    for command in procedure.split("\n") {
+        let mut command_parts = command.split(" ");
+        command_parts.next();
+        let package_count: usize = command_parts
+            .next()
+            .expect(CORRUPTED_FILE_MESSAGE)
+            .parse()
+            .expect(CORRUPTED_FILE_MESSAGE);
+        command_parts.next();
+        let from_stack_index: usize = command_parts
+            .next()
+            .expect(CORRUPTED_FILE_MESSAGE)
+            .parse()
+            .expect(CORRUPTED_FILE_MESSAGE);
+        let from_stack_index = from_stack_index - NUMBER_TO_INDEX;
+        command_parts.next();
+        let to_stack_index: usize = command_parts
+            .next()
+            .expect(CORRUPTED_FILE_MESSAGE)
+            .parse()
+            .expect(CORRUPTED_FILE_MESSAGE);
+        let to_stack_index = to_stack_index - NUMBER_TO_INDEX;
+
+        let from_stack = stacks
+            .get_mut(from_stack_index)
+            .expect(CORRUPTED_FILE_MESSAGE);
+        let mut packages = from_stack.split_off(from_stack.len() - package_count);
+        let to_stack = stacks
+            .get_mut(to_stack_index)
+            .expect(CORRUPTED_FILE_MESSAGE);
+        to_stack.append(&mut packages);
+    }
+
+    return stacks;
+}
+fn second_part(parsed_stacks: Vec<VecDeque<char>>, procedure: &str) -> String {
+    let stacks = operate_procedure_second_part(procedure, parsed_stacks);
+
+    let mut answer = String::new();
+
+    for mut stack in stacks {
+        let package = stack.pop_back().expect(CORRUPTED_FILE_MESSAGE);
+        answer.push(package)
+    }
+
+    return answer;
+}
 
 fn main() {
     let input = fs::read_to_string("input.txt").expect("Failed to read the file");
@@ -105,13 +172,8 @@ fn main() {
     let procedure = stacks_and_procedure.next().expect(CORRUPTED_FILE_MESSAGE);
 
     let parsed_stacks = parse_stacks(stacks);
-    let stacks = operate_procedure(procedure, parsed_stacks);
 
-    let mut answer = String::new();
+    let second_answer = second_part(parsed_stacks, procedure);
 
-    for mut stack in stacks {
-        let package = stack.pop_back().expect(CORRUPTED_FILE_MESSAGE);
-        answer.push(package)
-    }
-    println!("{answer}")
+    println!("second answer: {second_answer}")
 }
