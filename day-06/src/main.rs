abac7a6ec7571bafd743fef335757f0cebdd61f5
Roman <romagritcaenko@gmail.com>
2023-05-20 12:23:39 +0300

    day 6 part 2

--- day-06/src/main.rs
+++ day-06/src/main.rs
@@ -1,37 +1,14 @@
-use std::{collections::HashMap, fs, process::exit};
+use day_06::{get_index_of_marker_end, get_input};
 
 fn main() {
-    let input = fs::read_to_string("input.txt").expect("Failed to open file");
-    let chars: Vec<char> = input.chars().collect();
-    if chars.len() < 4 {
-        panic!("There should by at least 4 chars in the input!");
-    }
+    let chars = match get_input() {
+        Ok(chars) => chars,
+        Err(message) => panic!("Error while trying to get input: {message}"),
+    };
 
-    let mut start = 0;
+    let first_part_result = get_index_of_marker_end(&chars, 4);
+    println!("answer to the first part: {}", first_part_result + 1);
 
-    let mut char_hash: HashMap<char, usize> = HashMap::new();
-    loop {
-        let end = start + 3;
-        let buffer_slice = chars.get(start..=end).unwrap();
-        let mut all_unique = true;
-
-        for (index, char) in buffer_slice.iter().enumerate() {
-            match char_hash.insert(*char, index) {
-                Some(index_of_present) => {
-                    // shift to the next char after the duplicated one
-                    start = start + index_of_present + 1;
-                    all_unique = false;
-                }
-                _ => {}
-            }
-        }
-
-        if all_unique {
-            // convert index to real number
-            println!("The answer is {}", end + 1);
-            exit(0);
-        } else {
-            char_hash.clear();
-        }
-    }
+    let second_part_result = get_index_of_marker_end(&chars, 14);
+    println!("answer to the second part: {}", second_part_result + 1);
 }
