4a79777fbae5b10bda0df53bf430e62bbef2702d
Roman <romagritcaenko@gmail.com>
2023-08-10 20:08:26 +0300

    remove print

--- day-07/src/main.rs
+++ day-07/src/main.rs
@@ -3,7 +3,6 @@ use std::fs;
 use day_07::get_file_system;
 
 fn main() {
-    println!("test!!! What's up?");
     let log = fs::read_to_string("input.txt").expect("Failed to read the file");
 
     let file_system = get_file_system(log);
