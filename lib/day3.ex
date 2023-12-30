89e2d3330e448ebef5ed31e71a3889244a3ab171
Roman <romagritcaenko@gmail.com>
2023-12-30 21:49:56 +0200

    day 3 part one finish

--- lib/day3.ex
+++ lib/day3.ex
@@ -2,25 +2,67 @@ defmodule Day3 do
   def part_one do
     {:ok, content} = File.read("input3")
 
-    lines = content |> String.trim() |> String.split("\n")
+    lines_with_index = content |> String.trim() |> String.split("\n") |> Enum.with_index()
 
-    part_coordinates = get_part_coordinates(lines)
+    part_coordinates = get_part_coordinates(lines_with_index)
 
-    IO.inspect(part_coordinates, label: "===\npart coordinates")
+    numbers_with_parts = get_numbers_with_parts(lines_with_index, part_coordinates)
+
+    numbers_with_parts |> Enum.map(&String.to_integer/1) |> Enum.sum()
   end
 
-  defp get_part_coordinates(lines, index \\ 0, line_parts_map \\ %{})
-  defp get_part_coordinates([], _index, line_parts_map), do: line_parts_map
+  def get_numbers_with_parts(lines_with_index, part_coordinates, numbers_with_parts \\ [])
+  def get_numbers_with_parts([], _part_coordinates, numbers_with_parts), do: numbers_with_parts
 
-  defp get_part_coordinates([line | rest_lines], index, line_parts_map) do
+  def get_numbers_with_parts(
+        [{line, index} | rest_lines],
+        part_coordinates,
+        numbers_with_parts
+      ) do
+    filter_condition = fn number_position ->
+      has_part?(number_position, Map.get(part_coordinates, index - 1)) or
+        has_part?(number_position, Map.get(part_coordinates, index)) or
+        has_part?(number_position, Map.get(part_coordinates, index + 1))
+    end
+
+    numbers_with_parts_from_line =
+      Regex.scan(~r/\d+/, line, return: :index)
+      |> List.flatten()
+      |> Enum.filter(filter_condition)
+      |> Enum.map(fn number_position ->
+        String.slice(line, elem(number_position, 0), elem(number_position, 1))
+      end)
+
+    get_numbers_with_parts(
+      rest_lines,
+      part_coordinates,
+      numbers_with_parts_from_line ++ numbers_with_parts
+    )
+  end
+
+  defp has_part?({number_start, number_length}, index_list) do
+    if index_list === nil do
+      false
+    else
+      number_end = number_start + number_length - 1
+      range_to_check = (number_start - 1)..(number_end + 1)
+
+      Enum.any?(index_list, fn index -> index in range_to_check end)
+    end
+  end
+
+  def get_part_coordinates(lines_with_index, line_parts_map \\ %{})
+  def get_part_coordinates([], line_parts_map), do: line_parts_map
+
+  def get_part_coordinates([{line, index} | rest_lines], line_parts_map) do
     indexes_of_parts = line |> get_indexes_of_parts()
 
     if length(indexes_of_parts) > 0 do
       new_lines_map = Map.put(line_parts_map, index, indexes_of_parts)
 
-      get_part_coordinates(rest_lines, index + 1, new_lines_map)
+      get_part_coordinates(rest_lines, new_lines_map)
     else
-      get_part_coordinates(rest_lines, index + 1, line_parts_map)
+      get_part_coordinates(rest_lines, line_parts_map)
     end
   end
 
